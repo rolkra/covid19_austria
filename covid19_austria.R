fe37696dbf204f18cd7fa54f0aa57bf80759aeab
rolkra <31808268+rolkra@users.noreply.github.com>
2020-03-15 11:49:04 +0100

    Update covid19_austria.R

--- covid19_austria.R
+++ covid19_austria.R
@@ -2,9 +2,10 @@
 library(tidyverse)
 
 # reported infections (per week)
-infected <- c(2,2,3,5,7,10,
-              14,18,29,41,55,79,102,
-              131,182,246,361,504)
+# source: https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
+infected <- c(2,2,3,3,9,14,
+              18,21,29,41,55,79,104,
+              131,182,246,302,504)
 
 # prepare dataset
 data <- tibble(infected = infected)
@@ -36,7 +37,7 @@ predict_corona <- function(data, infection_rate, days)  {
 } # predict_corona
 
 # predict for different growth
-predict_days <- 57
+predict_days <- 50 #57
 
 data_50 <- data %>% 
   predict_corona(infection_rate = 1.50, days = predict_days)
@@ -61,15 +62,19 @@ data_plot <- data %>%
   bind_rows(data_50, data_40, data_33, data_20, data_15, data_10)
 
 # visualise
+last_day <- length(infected)
 data_plot %>% 
   mutate(infected_M = infected / 1000000) %>% 
   ggplot(aes(day, infected_M, color = type)) + 
   geom_line(size = 1.5) +
-  geom_vline(xintercept = c(length(infected), 
-                            length(infected) + 28), 
+  geom_vline(xintercept = c(last_day, last_day + 28), 
              linetype = "dotted") +
   ylim(0,5.5) +
   xlab("Days since outbreak") +
   ylab("Infected in Mio") + 
   ggtitle("Covid-19 outbreak in Austria") +
-  theme_minimal()
+  theme_minimal()+
+  annotate("text", last_day/2, 5, 
+           label = "until today", size = 2.5) +
+  annotate("text", last_day+14, 5, 
+           label = "next 4 weeks", size = 2.5)
