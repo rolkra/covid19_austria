16decf522b89c45d3c9b708e52c08d7465edcfe0
Krasser Roland <Roland.Krasser@A1.at>
2020-03-15 22:35:36 +0100

    Vienna

--- covid19_vienna.R
+++ covid19_vienna.R
@@ -1,15 +1,24 @@
 # packages
 library(tidyverse)
 
-# reported infections (per week)
-# source: https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
-infected <- c(2,2,3,3,9,14,
-              18,21,29,41,55,79,104,
-              131,182,246,302,504)
+# reported infections
+# source: APA OTS (https://www.ots.at)
+data <- tribble(
+  ~date,        ~infected,
+  "2020-03-05", 16,
+  "2020-03-06", 23,
+  "2020-03-07", 23,
+  "2020-03-08", 32,
+  "2020-03-09", 33,
+  "2020-03-10", 43,
+  "2020-03-11", 50,
+  "2020-03-12", 66,
+  "2020-03-13", 74,
+  "2020-03-14", 85,
+  "2020-03-15", 122
+)
 
-# prepare dataset
-data <- tibble(infected = infected)
-
+# prepare data
 data <- data %>% 
   mutate(type = "reported",
          day = row_number()) %>% 
@@ -19,7 +28,7 @@ data <- data %>%
 
 # predict
 predict_corona <- function(data, infection_rate, days)  {
-
+  
   future <- seq(from = max(data$day) + 1, to = max(data$day)+days-1)
   infected_all <- NULL
   infected_act <- max(data$infected)
@@ -61,20 +70,24 @@ data_10 <- data %>%
 data_plot <- data %>% 
   bind_rows(data_50, data_40, data_33, data_20, data_15, data_10)
 
+
+# visualise data
 # visualise
-last_day <- length(infected)
+last_day <- length(data$infected)
 data_plot %>% 
   mutate(infected_M = infected / 1000000) %>% 
-  ggplot(aes(day, infected_M, color = type)) + 
+  ggplot(aes(day, infected, color = type)) + 
   geom_line(size = 1.5) +
   geom_vline(xintercept = c(last_day, last_day + 28), 
              linetype = "dotted") +
-  ylim(0,5.5) +
+  ylim(0,1000) +
   xlab("Days since outbreak") +
-  ylab("Infected in Mio") + 
-  ggtitle("Covid-19 outbreak in Austria") +
+  ylab("Infected") + 
+  ggtitle("Covid-19 outbreak in Vienna") +
   theme_minimal()+
   annotate("text", last_day/2, 5, 
            label = "until today", size = 2.5) +
   annotate("text", last_day+14, 5, 
            label = "next 4 weeks", size = 2.5)
+
+
