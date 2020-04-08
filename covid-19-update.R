01af823c97a052e4acb2efda48ce6185cd2935aa
Krasser Roland <Roland.Krasser@A1.at>
2020-04-08 21:39:11 +0200

    no legend

--- covid-19-update.R
+++ covid-19-update.R
@@ -28,6 +28,27 @@ p <- (p1 | p2)
 
 p %>% covid19_save_plot("covid-19-deaths.png")
 
+# infected World
+p1 <- data %>% covid19_plot_infected("China/Hubei", title = "China/Hubei") + theme(legend.position = "none")
+p2 <- data %>% covid19_plot_infected("Korea, South", title = "Korea, South")+ theme(legend.position = "none")
+p3 <- data %>% covid19_plot_infected("Japan", title = "Japan")+ theme(legend.position = "none")
+p4 <- data %>% covid19_plot_infected("US", title = "US")+ theme(legend.position = "none")
+p5 <- data %>% covid19_plot_infected("Brazil", title = "Brazil")+ theme(legend.position = "none")
+p6 <- data %>% covid19_plot_infected("South Africa", title = "South Africa")+ theme(legend.position = "none")
+
+p <- (p1 | p2 | p3) / (p4 | p5 | p6)
+p %>% covid19_save_plot("covid-19-world-infected.png")
+
+p7 <- data %>% covid19_plot_infected("Italy", title = "Italy")+ theme(legend.position = "none")
+p8 <- data %>% covid19_plot_infected("Spain", title = "Spain")+ theme(legend.position = "none")
+p9 <- data %>% covid19_plot_infected("United Kingdom", title = "UK")+ theme(legend.position = "none")
+p10 <- data %>% covid19_plot_infected("Germany", title = "Germany")+ theme(legend.position = "none")
+p11 <- data %>% covid19_plot_infected("Netherlands", title = "Netherlands")+ theme(legend.position = "none")
+p12 <- data %>% covid19_plot_infected("Belgium", title = "Belgium")+ theme(legend.position = "none")
+
+p <- (p7 | p8 | p9) / (p10 | p11 | p12)
+p %>% covid19_save_plot("covid-19-europe-infected.png")
+
 # infected Austria
 p <- data %>% covid19_plot_infected("Austria")
 p %>% covid19_save_plot("covid-19-austria-infected.png")
